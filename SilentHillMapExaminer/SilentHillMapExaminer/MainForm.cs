a8a9149e0823b8947dee8d189bf2a41d54113fca
Rob Martens <robert.martens@gmail.com>
2020-10-15 05:56:30 -0400

    Add project files.

---
+++ SilentHillMapExaminer/SilentHillMapExaminer/MainForm.cs
@@ -0,0 +1,14 @@
+using System;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace SilentHillMapExaminer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
