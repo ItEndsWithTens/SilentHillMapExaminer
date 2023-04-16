8f828f5a37c74c2677b75efc3458564901724ce1
Rob Martens <robert.martens@gmail.com>
2023-04-15 20:06:28 -0400

    Run custom build task in its own process
    Frees up BizHawk.Common.dll (used for reading the version we're compiling against) after a build, where previously it was held open by MSBuild thanks to "node reuse", which can't be disabled without command line switches or environment variables.

---
+++ src/CustomBuildTasks/CustomBuildTasks.cs
@@ -0,0 +1,19 @@
+﻿using BizHawk.Common;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace CustomBuildTasks
+{
+	public class ReadBizHawkVersion : Task
+	{
+		[Output]
+		public string BHV { get; private set; } = "X.Y";
+
+		public override bool Execute()
+		{
+			BHV = VersionInfo.MainVersion;
+
+			return true;
+		}
+	}
+}
