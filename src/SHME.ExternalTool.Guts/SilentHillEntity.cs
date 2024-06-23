1cb63e08d02915d9e85a71b5111f71cfc71b13d4
Rob Martens <robert.martens@gmail.com>
2024-06-22 22:09:52 -0400

    Use file scoped namespace for SilentHillEntity
    An old file, but it's barely been touched, and has very little in it. Again, I feel confident this sort of change won't drastically affect long term diffs.

--- src/SHME.ExternalTool.Guts/SilentHillEntity.cs
+++ src/SHME.ExternalTool.Guts/SilentHillEntity.cs
@@ -1,16 +1,15 @@
-﻿namespace SHME.ExternalTool
+﻿namespace SHME.ExternalTool;
+
+public static class SilentHillEntitySizes
 {
-	public static class SilentHillEntitySizes
-	{
-		public static int CameraPath => 24;
-		public static int PointOfInterest => 12;
-		public static int Trigger => 12;
-	}
+	public static int CameraPath => 24;
+	public static int PointOfInterest => 12;
+	public static int Trigger => 12;
+}
 
-	public class SilentHillEntity
-	{
-		public virtual int SizeInBytes { get; }
+public class SilentHillEntity
+{
+	public virtual int SizeInBytes { get; }
 
-		public virtual long Address { get; protected set; }
-	}
+	public virtual long Address { get; protected set; }
 }
