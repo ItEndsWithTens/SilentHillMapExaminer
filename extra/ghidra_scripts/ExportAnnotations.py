b8c1d14fb36f11d1fe6e3d9c822da6f4d62eff99
Rob Martens <robert.martens@gmail.com>
2025-05-28 14:37:14 -0400

    Improve Ghidra export script
    [skip ci]

--- extra/ghidra_scripts/ExportAnnotations.py
+++ extra/ghidra_scripts/ExportAnnotations.py
@@ -6,17 +6,23 @@
 #@category Export
 #@runtime PyGhidra
 
-from enum import Enum
+# See https://pypi.org/project/ghidra-stubs for the origin of this snippet.
+import typing
+if typing.TYPE_CHECKING:
+    from ghidra.ghidra_builtins import *
 
-from ghidra.app.util import *
+import enum
+import os
+import sys
+
 from ghidra.program.model.symbol import SourceType, SymbolType
-from ghidra.app.script import AskDialog
+from ghidra.util.exception import CancelledException
 
 from java.lang import IllegalArgumentException
 
 
 
-class GhidraCommentStyle(Enum):
+class GhidraCommentStyle(enum.Enum):
     EOL = 0
     PRE = 1
     POST = 2
@@ -25,40 +31,32 @@ class GhidraCommentStyle(Enum):
 
 
 
+try:
+    outFile = askFile("Output file", "OK")
+except CancelledException:
+    print("Export cancelled, exiting.")
+    sys.exit(0)
+
 confirm = True
-outFile = askFile("Output file", "OK")
 if outFile.exists():
     confirm = askYesNo("File already exists!", "Overwrite existing file?")
 if confirm == False:
-    raise SystemExit("Specified output file already exists, exiting without exporting.")
-
-f_user_defined = []
-l_user_defined = []
-f_analysis = []
-l_analysis = []
+    print("Specified output file already exists, exiting without exporting.")
+    sys.exit(0)
 
 program = getCurrentProgram()
 symbol_table = program.getSymbolTable()
 symbols = symbol_table.getDefinedSymbols()
-
-for sym in symbols:
-    if sym.symbolType == SymbolType.LABEL:
-        tuple = (sym.address.toString(), 'label', sym.name)
-        if sym.source == SourceType.USER_DEFINED:
-            l_user_defined.append(tuple)
-        elif sym.source == SourceType.ANALYSIS:
-            l_analysis.append(tuple)
-    elif sym.symbolType == SymbolType.FUNCTION:
-        tuple = (sym.address.toString(), 'function', sym.name, sym.object.signature.toString())
-        if sym.source == SourceType.USER_DEFINED:
-            f_user_defined.append(tuple)
-        elif sym.source == SourceType.ANALYSIS:
-            f_analysis.append(tuple)
-
-comments = []
-
 factory = program.getAddressFactory()
 
+symbol_lists = []
+for sym in symbols:
+    list = [f'{sym.symbolType}', f'{sym.source}', f'{sym.address}', sym.name]
+    if sym.symbolType == SymbolType.FUNCTION:
+        list.append(f'{sym.object.signature}')
+    symbol_lists.append(list)
+
+comments = []
 for space in factory.getAllAddressSpaces():
     set = factory.getAddressSet(space.getMinAddress(), space.getMaxAddress())
 
@@ -72,33 +70,51 @@ for space in factory.getAllAddressSpaces():
             comment = program.listing.getComment(style.value, address)
             if comment is None:
                 continue
-            comments.append((address.toString(), style.name, comment))
+            comments.append(('Comment', f'{address}', comment, style.name))
+
+labels = [l for l in symbol_lists if l[0] == f'{SymbolType.LABEL}']
+functions = [l for l in symbol_lists if l[0] == f'{SymbolType.FUNCTION}']
+
+analysis_s = SourceType.ANALYSIS.toString()
+user_s = SourceType.USER_DEFINED.toString()
+
+# A GUID for such a primitive format may be overkill, but it's prudent, and
+# they're easy enough to generate. An existing format for this sort of data
+# would have been preferable, but curiously none seem to exist.
+FILE_FORMAT_GUID = '67035dec-e33b-4268-b074-f9cafc98295d'
+FILE_FORMAT_VERSION = 1
 
 with open(outFile.getAbsolutePath(), 'w') as of:
-    of.write('# Labels - ANALYSIS\n\n')
-    for tuple in l_analysis:
-        of.write(' '.join(tuple) + '\n')
+    of.write(f'FileFormatGuid {FILE_FORMAT_GUID}\n')
+    of.write(f'FileFormatVersion {FILE_FORMAT_VERSION}\n\n')
+
+    locator = getProjectRootFolder().getProjectLocator()
+    name = locator.getName()
+    marker = locator.getMarkerFile().toString()
+
+    of.write(f'ProjectName {name}\n')
+    of.write(f'ProjectDirectory {os.path.dirname(marker)}\n')
+    of.write(f'ProgramPath {program.getDomainFile().getPathname()}\n\n')
+
+    for label in [item for item in labels if item[1] == analysis_s]:
+        of.write(' '.join(label) + '\n')
 
     of.write('\n\n\n')
 
-    of.write('# Functions - ANALYSIS\n\n')
-    for tuple in f_analysis:
-        of.write(' '.join(tuple) + '\n')
+    for label in [item for item in labels if item[1] == user_s]:
+        of.write(' '.join(label) + '\n')
 
     of.write('\n\n\n')
 
-    of.write('# Labels - USER_DEFINED\n\n')
-    for tuple in l_user_defined:
-        of.write(' '.join(tuple) + '\n')
+    for function in [item for item in functions if item[1] == analysis_s]:
+        of.write(' '.join(function) + '\n')
 
     of.write('\n\n\n')
 
-    of.write('# Functions - USER_DEFINED\n\n')
-    for tuple in f_user_defined:
-        of.write(' '.join(tuple) + '\n')
+    for function in [item for item in functions if item[1] == user_s]:
+        of.write(' '.join(function) + '\n')
 
     of.write('\n\n\n')
 
-    of.write('# Comments - EOL, PRE, POST, PLATE, REPEATABLE\n\n')
     for tuple in comments:
-        of.write(' '.join(tuple) + '\n')
+        of.write(' '.join(tuple) + '\n\n')
